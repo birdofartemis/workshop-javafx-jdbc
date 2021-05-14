18fb6433c14b9eb1dccca8e03f0e7bc62e5d7b5f
birdofartemis <72250724+birdofartemis@users.noreply.github.com>
2021-05-14 14:09:14 +0000

    MainView Controller

---
+++ src/app/gui/MainViewController.java
@@ -0,0 +1,33 @@
+package app.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+    @FXML private MenuItem menuItemSeller;
+    @FXML private MenuItem menuItemDepartment;
+    @FXML private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
